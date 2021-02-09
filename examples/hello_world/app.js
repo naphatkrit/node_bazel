e2c1ee6468ecdd65b0dc820caf8a37ac50637034
Naphat Sanguansin <naphat.krit@gmail.com>
2021-02-09 14:07:07 +0000

    add example hello_world app to work with

---
+++ examples/hello_world/app.js
@@ -0,0 +1,15 @@
+const chalk = require('chalk')
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = process.env.PORT
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World!\n')
+})
+
+server.listen(port, hostname, () => {
+  console.log('Server running at ' + chalk.blue(`http://${hostname}:${port}/`))
+})
